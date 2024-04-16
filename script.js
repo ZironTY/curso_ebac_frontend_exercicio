c63b6de8ba536ec6266c880edf02195048f80f4d
ZironTY <162383605+ZironTY@users.noreply.github.com>
2024-04-16 19:23:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () { 
+  var taskForm = $("#task-form");
+  var taskList = $("#task-list");
+
+  taskForm.submit(function (event) {
+    event.preventDefault();
+
+    var taskName = $("#task").val();
+
+    
+    taskList.append("<li>" + taskName + "</li>");
+
+    
+    $("#task").val("");
+  });
+
+  taskList.on("click", "li", function () {
+    $(this).toggleClass("completed");
+  });
+});
